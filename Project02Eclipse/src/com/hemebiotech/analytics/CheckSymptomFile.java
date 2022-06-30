2f0cacd19f18a2e00b66f41733319c9078d890ff
Christophe <christophe.becquey@leroymerlin.fr>
2022-06-30 10:57:06 +0200

    fix(analyticsCounter): add results

--- Project02Eclipse/src/com/hemebiotech/analytics/CheckSymptomFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/CheckSymptomFile.java
@@ -8,7 +8,6 @@ public class CheckSymptomFile {
     // Vérifier si le fichier d'entrée existe, s'il n'existe pas on renvoie un message d'erreur
     public String getFilePathFromClassPath(String fileName) throws FileNotFoundException {
         URL resource = this.getClass().getResource(fileName);
-        System.out.println(fileName);
         if (resource != null)
             return resource.getPath();
         throw new FileNotFoundException("File " + fileName + " not found");
